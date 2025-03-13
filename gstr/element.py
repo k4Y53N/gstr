e63b9305754ba49e28949611d3ab6a3342173330
k4Y53N <ks93041@gmail.com>
2025-03-13 10:01:45 +0800

    element support  mluti src and sink

--- gstr/element.py
+++ gstr/element.py
@@ -6,114 +6,97 @@ __all__ = ['RawElement', 'GstElement']
 
 
 class Element(ABC):
-    """Abstract base class for elements in the pipeline.
-
-    Attributes:
-        link (BaseElement): The next element in the pipeline.
-    """
-
-    link: 'Element' = None
-    name: Optional[str] = None
-    srcs: Optional[List['Element']] = None
-    sinks: Optional[List['Element']] = None
-
-    def T(self) -> str:
-        """Return the element type.
-
-        Raises:
-            NotImplementedError: Not implemented.
-
-        Returns:
-            str: The element type.
-        """
-        return self.__class__.__name__.lower()
-
-    @abstractmethod
-    def get_properties(self) -> Dict[str, Any]:
-        """Return the element properties.
-
-        Raises:
-            NotImplementedError: Not implemented.
-
-        Returns:
-            Dict[str, Any]: The element properties.
-        """
-        raise NotImplementedError()
+    __srcs: Optional[List['Element']] = None
+    __sinks: Optional[List['Element']] = None
 
     def __str__(self) -> str:
         return self.build()
 
-    def __or__(self, other: 'Element') -> 'Element':
-        """Link the current element to another element.
-
-        Args:
-            other (BaseElement): The element to link to.
-
-        Returns:
-            BaseElement: The current element.
-        """
-        if self.link is None:
-            self.link = other
-        else:
-            self.link.__or__(other)
-
-        return self
-
-    def get_srcs(self) -> List['Element']:
-        """Return the element sources."""
-        if self.srcs is None:
-            self.srcs = []
-
-        return self.srcs
-
-    def get_sinks(self) -> List['Element']:
-        """Return the element sinks."""
-        if self.sinks is None:
-            self.sinks = []
-
-        return self.sinks
-
-    def get_name(self) -> str:
-        """Return the element name."""
-        if self.name is not None:
-            return self.name
-
-        return self.T().upper()
-
-    def set_name(self, name: str) -> 'Element':
-        """Set the element name.
-
-        Args:
-            name (str): The element name.
-
-        Returns:
-            BaseElement: The current element.
-        """
-        self.name = name
-
-        return self
-
-    def build(self) -> str:
-        """Build the element string representation.
-
-        Returns:
-            str: The element string representation.
-        """
-        properties = self.build_properties()
+    def __repr__(self):
+        with_name = max(len(self.srcs), len(self.sinks)) > 1
+        properties = self.build_properties(with_name=with_name)
         element = self.separate().join([self.T(), *properties])
 
-        if self.link is None:
-            return element
+        return element
 
-        return f'{element} ! {self.link}'
+    def __or__(self, other: 'Element') -> 'Element':
+        if self.sinks:
+            self.sinks[-1].__or__(other)
+        else:
+            self.sinks.append(other)
+            other.srcs.append(self)
 
-    def build_properties(self) -> List[str]:
-        """Build the element properties string representation.
+        return self
 
-        Returns:
-            List[str]: The list of properties strings.
-        """
+    def __mul__(self, other: 'Element') -> 'Element':
+        self.sinks.append(other)
+        other.srcs.append(self)
+
+        return self
+
+    @abstractmethod
+    def get_properties(self) -> Dict[str, Any]:
+        raise NotImplementedError()
+
+    def build(self, other: Optional['Element'] = None) -> str:
+        len_srcs = len(self.srcs)
+        len_sinks = len(self.sinks)
+        with_name = max(len_srcs, len_sinks) > 1
+        element_appeneded = False
+        properties = self.build_properties(with_name=with_name)
+        element = self.separate().join([self.T(), *properties])
+        links: List[str] = []
+
+        for src in self.srcs:
+            if src is other:
+                continue
+
+            if len_srcs > 1:
+                links.append(f'{src.build(self)} ! {self.name}.')
+            else:
+                links.append(f'{src.build(self)} ! {element}')
+                element_appeneded = True
+
+        if not element_appeneded:
+            links.append(element)
+
+        for sink in self.sinks:
+            if sink is other:
+                continue
+
+            if len_sinks > 1:
+                links.append(f'{self.name}. ! {sink.build(self)}')
+            else:
+                links.append(f'! {sink.build(self)}')
+
+        return ' '.join(links)
+
+    def T(self) -> str:
+        return self.__class__.__name__.lower()
+
+    @property
+    def name(self) -> Optional[str]:
+        return self.__class__.__name__.upper()
+
+    @property
+    def srcs(self) -> List['Element']:
+        if self.__srcs is None:
+            self.__srcs = []
+
+        return self.__srcs
+
+    @property
+    def sinks(self) -> List['Element']:
+        if self.__sinks is None:
+            self.__sinks = []
+
+        return self.__sinks
+
+    def build_properties(self, with_name=False) -> List[str]:
         properties = self.get_properties()
+
+        if with_name:
+            properties['name'] = self.name
 
         return [
             f'{self.clean_property_key(k)}={self.clean_property_value(v)}'
@@ -122,36 +105,15 @@ class Element(ABC):
         ]
 
     def separate(self) -> str:
-        """Return the separator for the element properties.
-
-        Returns:
-            str: The separator.
-        """
         return ' '
 
     def clean_property_key(self, key: str) -> str:
-        """Clean the property key.
-
-        Args:
-            key (str): The property key.
-
-        Returns:
-            str: The cleaned property key.
-        """
         if key.startswith('_'):
             key = key[1:]
 
         return str(key).replace('_', '-')
 
     def clean_property_value(self, value: Any) -> str:
-        """Clean the property value.
-
-        Args:
-            value (Any): The property value.
-
-        Returns:
-            str: The cleaned property value.
-        """
         if isinstance(value, bool):
             return str(value).lower()
 
@@ -160,12 +122,6 @@ class Element(ABC):
 
 class RawElement(Element):
     def __init__(self, E: str, **kwargs):
-        """Create a new raw element.
-
-        Args:
-            E (str): The element type.
-            **kwargs: The element properties
-        """
         self.E = E
         self.properties = kwargs
 
