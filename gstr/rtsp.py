84af7293bf787f2d63ea1648e52ab1c84557b913
k4Y53N <ks93041@gmail.com>
2025-11-20 10:26:59 +0800

    add rtspclientsink

--- gstr/rtsp.py
+++ gstr/rtsp.py
@@ -5,10 +5,17 @@ from .element import GstElement
 
 __all__ = [
     'RTSPSrc',
+    'RTSPClientSink',
 ]
 
 
 @dataclass
 class RTSPSrc(GstElement):
+    location: str
+    protocols: Literal['tcp', 'udp', 'udp-mcast' 'tcp+udp-mcast+udp'] | None = None
+
+
+@dataclass
+class RTSPClientSink(GstElement):
     location: str
     protocols: Literal['tcp', 'udp', 'udp-mcast' 'tcp+udp-mcast+udp'] | None = None
