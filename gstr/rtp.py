a862f67b46f00f51d31215a16785f2b7554b1d39
k4Y53N <ks93041@gmail.com>
2026-04-08 12:20:53 +0800

    RtpH264

--- gstr/rtp.py
+++ gstr/rtp.py
@@ -3,14 +3,14 @@ from dataclasses import dataclass
 from .element import GstElement
 
 __all__ = [
-    'Rtph264Pay',
-    'Rtph264Depay',
+    'RtpH264Pay',
+    'RtpH264Depay',
 ]
 
 
 @dataclass
-class Rtph264Pay(GstElement): ...
+class RtpH264Pay(GstElement): ...
 
 
 @dataclass
-class Rtph264Depay(GstElement): ...
+class RtpH264Depay(GstElement): ...
